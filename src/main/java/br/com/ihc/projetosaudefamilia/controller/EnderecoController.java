30c0498659c000cb58072fc1a88f26b3b19ebc3b
Felipe Aleixo <aleixofp@gmail.com>
2022-06-14 02:44:46 -0300

    descrição de alguns metodos

--- src/main/java/br/com/ihc/projetosaudefamilia/controller/EnderecoController.java
+++ src/main/java/br/com/ihc/projetosaudefamilia/controller/EnderecoController.java
@@ -2,6 +2,7 @@ package br.com.ihc.projetosaudefamilia.controller;
 
 import br.com.ihc.projetosaudefamilia.service.CEPService;
 import br.com.ihc.projetosaudefamilia.vo.EnderecoVO;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ public class EnderecoController {
     private CEPService cepService;
 
     @GetMapping("/consultar-cep/{cep}")
+    @ApiOperation(value = "Consulta o CEP informado e retorna o endereço completo", response = EnderecoVO.class)
     public ResponseEntity<EnderecoVO> consultarCep(@PathVariable("cep") String cep) throws IOException {
         var result = this.cepService.consultarCEP(cep);
         return ResponseEntity
--- src/main/java/br/com/ihc/projetosaudefamilia/controller/PessoaController.java
+++ src/main/java/br/com/ihc/projetosaudefamilia/controller/PessoaController.java
@@ -3,6 +3,7 @@ package br.com.ihc.projetosaudefamilia.controller;
 import br.com.ihc.projetosaudefamilia.service.CPFService;
 import br.com.ihc.projetosaudefamilia.service.PessoaService;
 import br.com.ihc.projetosaudefamilia.vo.PessoaVO;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ public class PessoaController {
     private PessoaService pessoaService;
 
     @GetMapping("/validar-cpf/{cpf}")
+    @ApiOperation(value = "Valida o CPF informado. A validação é em duas etapas: primeiro valida se o CPF está correto, utilizando do algoritmo; e em segundo valida se o CPF já foi cadastrado para uma outra pessoa.", response = Boolean.class)
     public ResponseEntity<Boolean> validarCpf(@PathVariable("cpf") String cpf){
         var result = this.cpfService.validarCpf(cpf) && !this.pessoaService.existePessoaComCpf(cpf);
         return ResponseEntity
